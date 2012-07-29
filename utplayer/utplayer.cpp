29e67970e8fda1752239d9c19aa656074f111ae8
UMEZAWA Takeshi <umezawa@utsoft.org>
2012-07-29 12:53:19 +0000

    アクセラレータを追加して 再生/一時停止・前後移動 を追加した。

--- utplayer/utplayer.cpp
+++ utplayer/utplayer.cpp
@@ -12,16 +12,22 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 {
 	CUtPlayerFrameWindow frame;
 	MSG msg;
+	HACCEL hAccel;
 
 	CoInitializeEx(NULL, COINIT_MULTITHREADED);
+
+	hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_UTPLAYER));
 
 	frame.Create(NULL, CWindow::rcDefault, "Ut Alpha Video Player");
 	frame.ShowWindow(nCmdShow);
 
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		if (!TranslateAccelerator(frame.m_hWnd, hAccel, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
 	}
 
 	return (int)msg.wParam;
